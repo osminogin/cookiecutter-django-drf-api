162ccd0fb9ef2664a6d4a9d03230ef5e9458cf7d
Vladimir Osintsev <oc@co.ru>
2017-08-19 15:47:30 +0300

    MIDDLEWARE settings fixed

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings_local.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings_local.py
@@ -7,10 +7,10 @@ ALLOWED_HOSTS = ['127.0.0.1', '::1']
 # Order mater!
 INSTALLED_APPS = ['debug_toolbar', 'livereload'] + INSTALLED_APPS
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'livereload.middleware.LiveReloadScript',   # Order mater!
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-] + MIDDLEWARE_CLASSES
+] + MIDDLEWARE
 
 DEBUG_TOOLBAR_CONFIG = {
     'JQUERY_URL': '',  # Avoid double jQuery load
