dd6b22bb3820217d73e337e9adcca6428168a5a7
Vladimir Osintsev <oc@co.ru>
2017-08-19 18:59:54 +0300

    Local settings load first if mangement script

--- {{cookiecutter.project_slug}}/manage.py
+++ {{cookiecutter.project_slug}}/manage.py
@@ -3,16 +3,24 @@ import os
 import sys
 
 if __name__ == '__main__':
-
+    # Loads environment variables from .env file
     try:
         import dotenv   # noqa
         dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
     except ImportError:
         pass
 
-    os.environ.setdefault('PYTHONUNBUFFERED', '1')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          '{{cookiecutter.project_slug}}.settings')
+    # Use settings_local.py config first and then default settings
+    try:
+        settings_local_path = os.path.join(
+            os.path.dirname(__file__), '{{cookiecutter.project_slug}}', 'settings_local.py'
+        )
+        assert os.path.isfile(settings_local_path)
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              '{{cookiecutter.project_slug}}.settings_local')
+    except AssertionError:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                              '{{cookiecutter.project_slug}}.settings')
 
     try:
         from django.core.management import execute_from_command_line    # noqa
@@ -27,4 +35,5 @@ if __name__ == '__main__':
             )
         raise
 
+    os.environ.setdefault('PYTHONUNBUFFERED', '1')
     execute_from_command_line(sys.argv)
